818e010b26d8fbe665e10adbb8a55a532abd11f7
Teppo Jouttenus <teppo.jouttenus@gmail.com>
2015-08-07 14:44:57 -0400

    json analysis scripts

--- scripts/analyze_segmentio.py
+++ scripts/analyze_segmentio.py
@@ -8,42 +8,79 @@ import zorba_api
 import cPickle as pickle
 from pprint import pprint
 
-# load data
-events = pickle.load(open("/Volumes/Analytics/Segment.io-logs/events_test.p", "rb"))
+def runZorba(query):
+    # load zorba
+    store = zorba_api.InMemoryStore_getInstance()
+    zorba = zorba_api.Zorba_getInstance(store)
 
-# print json.dumps(events[1])
-# pprint(events[0])
+    # run zorba
+    xquery = zorba.compileQuery(query)
+    output = xquery.execute()
 
-# load zorba
-store = zorba_api.InMemoryStore_getInstance()
-zorba = zorba_api.Zorba_getInstance(store)
+    # shut down zorba, free up memory
+    xquery = None
+    zorba.shutdown()
+    zorba_api.InMemoryStore_shutdown(store)
 
-# create zorba query
-eventString = json.dumps(events[0])
-for i in range(1,50):
-    eventString += ', '+json.dumps(events[i])
+    return output
 
-query = '''let $events := [''' + eventString + \
-''']
-let $join :=
-  for $event in jn:members($events)
-  where $event("event") = "Entered Section"
-  return {
-    "userID" : $event("userId"),
-    "Path Title" : $event("properties")("Path Title"),
-    "Module Title" : $event("properties")("Module Title"),
-    "Section Title" : $event("properties")("Section Title")
-  }
-return [$join]
-'''
+def createEnteredSectionQuery(eventString, sectionName):
+    query = '''let $events := ''' + eventString + \
+    '''
+    let $join :=
+      for $event in jn:members($events)
+      where $event("event") = "Entered Section"
+      where $event("properties")("Path Title") = "JavaScript Backends with Node.js"
+      where $event("properties")("Module Title") = "A re-introduction to JavaScript (JS tutorial)"
+      where $event("properties")("Section Title") = "'''+sectionName+'''"
+      return {
+        "userID" : $event("userId"),
+        "Path Title" : $event("properties")("Path Title"),
+        "Module Title" : $event("properties")("Module Title"),
+        "Section Title" : $event("properties")("Section Title")
+      }
+    return [$join]
+    '''
+    return query
+
+# load data as JSON
+events = pickle.load(open("/Volumes/Analytics/Segment.io-logs/eventsAsJson.p", "rb"))
+
+# load data as string
+with open("/Volumes/Analytics/Segment.io-logs/eventsAsString.json", ) as f:
+    eventString = f.read()
+
+pprint(events[3])
+
+# # old way
+# eventString2 = json.dumps(events[0])
+# for i in range(1,len(events)):
+#     eventString2 += ', '+json.dumps(events[i])
+# eventString2 = '['+eventString2+']'
+
+
+
+
 
 
 # run zorba query
-xquery = zorba.compileQuery(query)
-# print xquery.execute()
-pprint(json.loads(xquery.execute()))
-
-# shut down zorba, free up memory
-xquery = None
-zorba.shutdown()
-zorba_api.InMemoryStore_shutdown(store)
+query = createEnteredSectionQuery(eventString, "Introduction")
+queryResult = json.loads(runZorba(query))
+pprint(queryResult)
+print len(queryResult)
+query = createEnteredSectionQuery(eventString, "Overview")
+queryResult = json.loads(runZorba(query))
+pprint(queryResult)
+print len(queryResult)
+query = createEnteredSectionQuery(eventString, "Numbers")
+queryResult = json.loads(runZorba(query))
+pprint(queryResult)
+print len(queryResult)
+query = createEnteredSectionQuery(eventString, "Strings")
+queryResult = json.loads(runZorba(query))
+pprint(queryResult)
+print len(queryResult)
+query = createEnteredSectionQuery(eventString, "Other types")
+queryResult = json.loads(runZorba(query))
+pprint(queryResult)
+print len(queryResult)
--- scripts/pickle_segmentio.py
+++ scripts/pickle_segmentio.py
@@ -2,19 +2,37 @@
 
 # -*- coding: utf-8 -*-
 import os
+import re
 import json
 import cPickle as pickle
+
+# set up regexs
+
+reTrue = re.compile(r'(?<=":)true')
+reFalse = re.compile(r'(?<=":)false')
+reNull = re.compile(r'(?<=":)null')
 
 # Process data
 eventFilePath = '/Volumes/Analytics/Segment.io-logs/Unpacked'
 eventFiles = os.listdir(eventFilePath)
-events = []
+eventsAsJson = []
+eventsAsString = ""
 
 for f in eventFiles:
     with open(eventFilePath+'/'+f) as f:
         eventLines = f.readlines()
-        for i in range(len(eventLines)):
-            eventLines[i] = eventLines[i].replace('null', '\"None\"')
-            events.append(json.loads(eventLines[i]))
+        for i in range(len(eventLines)): # clean up segment.io notation to cleaner JSON
+            eventLines[i] = eventLines[i].replace('&', '&amp;')
+            eventLines[i] = reNull.sub(r'"None"', eventLines[i])
+            eventLines[i] = reTrue.sub(r'"True"', eventLines[i])
+            eventLines[i] = reFalse.sub(r'"False"', eventLines[i])
+            eventLines[i] = eventLines[i].replace('\\\"', '&quot;')
+            eventsAsString += ', ' + eventLines[i]
+            eventsAsJson.append(json.loads(eventLines[i]))
 
-pickle.dump(events, open("/Volumes/Analytics/Segment.io-logs/events_test.p", "wb"))
+eventsAsString = '['+eventsAsString[1:]+']'
+
+pickle.dump(eventsAsJson, open("/Volumes/Analytics/Segment.io-logs/eventsAsJson.p", "wb"))
+
+with open("/Volumes/Analytics/Segment.io-logs/eventsAsString.json", 'w+') as f:
+    f.write(eventsAsString)
